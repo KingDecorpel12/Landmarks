b56c5076a33d02501d1366d34012ea4c404e0354
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2021-11-15 15:05:30 -0500

    Add files via upload

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Omar Hegazy on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
