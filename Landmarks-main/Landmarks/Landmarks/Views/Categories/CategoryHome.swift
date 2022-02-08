b0c44b913b364f3d4830c27da81e2de7caa5739a
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2022-02-08 12:35:26 -0500

    Add files via upload

---
+++ Landmarks-main/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,43 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Omar Hegazy on 2/4/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    
+    var body: some View {
+        NavigationView
+        {
+        List
+            {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self)
+                {
+                    key in CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+    }
+  }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
+
---
+++ Landmarks-main/Landmarks/Landmarks/Views/Categories/CatgeoryItem.swift
@@ -0,0 +1,36 @@
+//
+//  CatgeoryItem.swift
+//  Landmarks
+//
+//  Created by Omar Hegazy on 2/5/22.
+//
+
+import SwiftUI
+
+struct CatgeoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View
+    {
+        VStack(alignment: .leading)
+        {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+          }
+        .padding(.leading, 15)
+        }
+    }
+
+struct CategoryItem_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        CatgeoryItem(landmark: ModelData().landmarks[0])
+    }
+}
